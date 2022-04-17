9308ac48c8cd9beed5afcc844cf4d1083a55b0ff
qjx321 <3255464687@qq.com>
2022-04-18 01:54:19 +0800

    remove some test code

--- src/vm.cpp
+++ src/vm.cpp
@@ -8,18 +8,12 @@ int main()
     VM vm(filename);
     Instruct instruct;
 
-    // cout << vm.get_code_size() << endl;
-    // vm.print_code();
-    // cout << "\n";
 
-    // for (uint32_t i =0; i < (vm.get_code_size())/3; i++)
     while(1)
     {
         instruct = vm.read_instruct();
-        // printf("\ninstruct: %08x %08x %08x\n", instruct.opcode, instruct.left, instruct.right);
+
         vm.execute(instruct);
-        // vm.print_stack();
-        // vm.print_regs();
     }
     vm.print_data();
 
